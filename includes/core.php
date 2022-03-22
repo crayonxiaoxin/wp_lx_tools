ed59c9cdaa1ee7f4ccee92f99f12087732073038
Lau <lau@efortunetech.com>
2022-03-22 09:55:59 +0800

    add update actions

--- includes/core.php
+++ includes/core.php
@@ -41,6 +41,16 @@ if (lx_tools_option(LxToolsFileds::$diabled_auto_update)) {
     wp_clear_scheduled_hook('wp_maybe_auto_update');
     // 移除 后台内核 更新检查
     remove_action( 'admin_init', '_maybe_update_core' );
+    // 移除 后台插件 更新检查 
+    remove_action( 'load-plugins.php', 'wp_update_plugins' );
+    remove_action( 'load-update.php', 'wp_update_plugins' );
+    remove_action( 'load-update-core.php', 'wp_update_plugins' );
+    remove_action( 'admin_init', '_maybe_update_plugins' );
+    // 移除 后台主题 更新检查 
+    remove_action( 'load-themes.php', 'wp_update_themes' );
+    remove_action( 'load-update.php', 'wp_update_themes' );
+    remove_action( 'load-update-core.php', 'wp_update_themes' );
+    remove_action( 'admin_init', '_maybe_update_themes' );
 }
 // 禁用WP更新提示
 if (lx_tools_option(LxToolsFileds::$hide_update_tips)) {
